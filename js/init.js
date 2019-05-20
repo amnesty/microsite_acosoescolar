63fc78635dcdf274061f077ccb18816c3a783d69
Estresarte <online@estresarte.com>
2019-05-20 13:40:27 +0200

    First Commit
    Solo maqueta HTML

---
+++ js/init.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+
+  	items: 3,
+  	dots:true,
+  	loop:true,
+  	responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+
+  });
+});
